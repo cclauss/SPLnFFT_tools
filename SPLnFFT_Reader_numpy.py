df8a18446f1117e276b9fe39e90ef2cf7faafcdf
cclauss <cclauss@bluewin.ch>
2015-08-18 12:37:23 +0200

    Properly deal with stripped files that have < 24h

--- SPLnFFT_Reader_numpy.py
+++ SPLnFFT_Reader_numpy.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# See: http://omz-forums.appspot.com/pythonista/post/6389848566923264
-# This code is available at: https://github.com/cclauss/uncategorized_hacks
+# See: https://forum.omz-software.com/topic/1964/binary-files-read-and-write
+# This code is available at: https://github.com/cclauss/SPLnFFT_tools
 
 # SPLnFFT app makes two sound pressure level readings every 1/8th of a second.
 # It stores the results into a daily binary data file that this script reads.
@@ -17,7 +17,18 @@
 
 import datetime, numpy
 
-filename = 'SPLnFFT_2015_07_21.bin'
+#filename = 'SPLnFFT_2015_07_21.bin'
+#filename = 'SPLnFFT_2015_08_17.bin'
+filename = 'SPLnFFT_2015_08_17_00h_to_08h.bin'
+try:
+    pieces = filename.split('_')
+    start_hour = int(pieces[4].rstrip('h'))
+    end_hour = int(pieces[6].rstrip('h.bin'))
+except IndexError:
+    start_hour, end_hour = 0, 24
+hours_of_data = end_hour - start_hour
+fmt = '{} hours of data from {:02}h to {:02}h'
+print(fmt.format(hours_of_data, start_hour, end_hour))
 
 def elapsed_time(msg='total'):
     return 'Elapsed time ({}): {}'.format(msg, datetime.datetime.now() - start)
@@ -25,10 +36,8 @@ def elapsed_time(msg='total'):
 start = datetime.datetime.now()
 data = numpy.fromfile(filename, dtype=numpy.float32).reshape(-1, 2)
 print(elapsed_time('4 Read and reshape'))
-print(type(data), len(data))  # numpy.ndarray, 691200
-#print(data[:20])  # print first 20 fast, slow pairs
-#print(data[-20:])  # print last 20 fast, slow pairs
-t = numpy.linspace(0.0, 24.0, len(data))
+print(type(data), len(data))  # numpy.ndarray, 691200 for a 24 hour file
+t = numpy.linspace(start_hour, end_hour, len(data))
 
 import matplotlib.pyplot as plt
 fig = plt.figure()
@@ -41,15 +50,17 @@ ax.plot(t[slow>0], slow[slow>0], 'ms-')
 t_bad = t[(fast <= 0) | (slow <= 0)]
 print(elapsed_time('3 Starting scatter...'))  # approx. 0.5 seconds
 ax.scatter(t_bad, max(fast.max(), slow.max()) + numpy.ones_like(t_bad), marker='o')
-print(elapsed_time('2 Scatter'))  # approx. 22 seconds
+print(elapsed_time('2 Scatter'))  # approx. 22 seconds for a 24 hour file
 plt.legend(('Fast Lp','Slow Lp','Readings <= 0'), loc='lower right')
 plt.title('Sound Pressure Level data from ' + filename)
-plt.xlabel('Eight samples per second across a 24 hour day')
+hours_of_data = end_hour - start_hour
+if hours_of_data == 24:
+    plt.xlabel('Eight samples per second across a 24 hour day')
+else:
+    plt.xlabel('Eight samples per second across {} hours'.format(hours_of_data))
 plt.ylabel('Sound Pressure Level (Lp) in dB(A)')
-# x axis currently starts at -5 and ends at 30 with ticks every 5 hours.
-# we would like it to starts at 0 and ends at 24 with a tick every hour.
-#plt.xticks(fast, xrange(24))  # crashes!
-#plt.xticks(fast, numpy.arange(0,24))  # crashes!
+plt.xlim(start_hour, end_hour)
+plt.xticks(numpy.arange(start_hour, end_hour))
 print(elapsed_time('1 Adornments'))
 plt.show()
-print(elapsed_time('0 plt.show() Done.'))  # approx. 2 minutes 20 seconds
+print(elapsed_time('0 plt.show() Done.'))  # approx. 2 minutes 20 seconds for a 24 hour file
